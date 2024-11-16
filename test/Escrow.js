c258de8b74cc6eddc0c84308bdca8ab8902de6b9
magistrigusti <magisrigusti@gmail.com>
2024-11-16 16:42:22 +0200

    0.1

--- test/Escrow.js
+++ test/Escrow.js
@@ -6,10 +6,41 @@ const tokens = (n) => {
 }
 
 describe('Escrow', () => {
+    let buyer, seller, inspector, lender;
+    let realEstate, escrow;
+
     it("save the adresses", async () => {
+        [buyer, seller, inspector, lender] = await ethers.getSigners();
+       
+
         const RealEstate = await ethers.getContractFactory('RealEstate');
         realEstate = await RealEstate.deploy();
 
-        console.log(realEstate.address);
+        let transaction = await realEstate.mint("https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1.png");
+        await transaction.wait();
+
+        const Escrow = await ethers.getContractFactory('Escrow');
+        escrow = await Escrow.deploy(
+            realEstate.address,
+            seller.address,
+            inspector.address,
+            lender.address
+        )
     })
 })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// "image": "https://ipfs.io/ipfs/QmQUozrHLAusXDxrvsESJ3PYB3rUeUuBAvVWw6nop2uu7c/1.png",
